78c97d31587fb5b167f8243bec36b8851713c27d
Sam Dutton <samdutton@gmail.com>
2019-01-04 10:43:03 +0000

    Updated code to new Lighthouse example

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const chromeLauncher = require('chrome-launcher');
 const fs = require('fs');
 const lighthouse = require('lighthouse');
-// const log = require('lighthouse-logger');
 
-const FLAGS = {
+const OPTIONS = {
   chromeFlags: ['--headless'],
   logLevel: 'info'
 };
@@ -29,29 +28,29 @@ for (let pageData of allPageData) {
 }
 
 // Launch Chrome, run a Lighthouse audit, then kill Chrome.
-function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
-  return chromeLauncher.launch(flags).then(chrome => {
-    flags.port = chrome.port;
-    return lighthouse(url, flags, config)
-      .then(results => chrome.kill().then(() => results));
+// Code is from https://github.com/GoogleChrome/lighthouse
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+    opts.port = chrome.port;
+    return lighthouse(url, opts, config).then(results => {
+      return chrome.kill().then(() => results.lhr)
+    });
   });
 }
-
-// log.setLevel(flags.logLevel);
 
 // Run a Lighthouse audit for a web page.
 // pageData is a CSV string beginning with a URL.
 // For example: https://johnlewis.com,John Lewis,homepage
 function audit(pageData) {
   const url = pageData.split(',')[0];
-  launchChromeAndRunLighthouse(url, FLAGS).then(results => {
-    const runtimeErrorMessage = results.lhr.runtimeError.message;
+  launchChromeAndRunLighthouse(url, OPTIONS).then(results => {
+    const runtimeErrorMessage = results.runtimeError.message;
     if (runtimeErrorMessage) {
       console.error(`\n>>>>>>> Runtime error for ${url}\n`);
       fs.appendFileSync(ERROR, 
         `Runtime error for ${url}: ${runtimeErrorMessage}\n\n`);
     } else {
-      const categories = Object.values(results.lhr.categories);
+      const categories = Object.values(results.categories);
       let scores = [];
       for (let category of categories) {
         scores.push(category.score);
