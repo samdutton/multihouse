eec27dd9bb89d000a0d2534c449b8c685d6b0e21
Sam Dutton <samdutton@gmail.com>
2019-01-15 18:48:34 +0000

    Updated to enable multiple runs with median values

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const lighthouse = require('lighthouse');
 const OPTIONS = {
   chromeFlags: ['--headless'],
   // logLevel: 'info'
+  onlyCategories: ['performance', 'seo']
 };
 
 const APPEND_OUTPUT = true;
@@ -25,7 +26,26 @@ const ERROR = 'error.txt';
 const INPUT = 'input.csv';
 const OUTPUT = 'output.csv';
 
-// Delete existing output and error data.
+const NUM_RUNS = 3;
+
+let pageIndex = 0;
+let runIndex = 0;
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   // TODO: Log the answer in a database
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
+
+// If required, delete existing output and error data.
 if (!APPEND_OUTPUT) {
   fs.writeFile(OUTPUT, '', () => {
     console.log('Deleted old output data\n');
@@ -39,21 +59,30 @@ fs.writeFile(ERROR, '', () => {
 // Each line in INPUT begins with a URL followed (optionally) by other CSV data.
 // For example: https://johnlewis.com,John Lewis,homepage
 const inputFileText = fs.readFileSync(INPUT, 'utf8').trim();
+// Note that no checks are done on the validity of inputData.
+const inputData = inputFileText.split('\n');
 
-audit(inputFileText.split('\n'));
+let data = [];
+audit(inputData);
 
 // Run a Lighthouse audit for a web page.
+// data is data for all audit runs for all pages
 // pages is an array of CSV strings, each ending with a URL.
 // For example: John Lewis,homepage,https://johnlewis.com
 function audit(pages) {
-  // Each page corresponds to line in the CSV file INPUT.
-  const page = pages.pop();
-  // Don't audit if this line in INPUT is blank or a comment.
-  if (page === '' || page[0] === '#') {
-    return
-  }
-  // Note that the split() call below doesn't work if URLs have commas
+  console.log('runIndex:', runIndex, 'pageIndex:', pageIndex,
+    'pages.length:', pages.length);
+  // page corresponds to a line of data in the CSV file INPUT.
+  const page = pages[pageIndex];
+  // The page URL is the last item on each line of CSV data.
+  // Note that split() in the line below doesn't work if URLs have commas.
   const url = page.split(',').slice(-1)[0];
+  // data is an array of objects: metadata and scores for each URL.
+  if (!data[pageIndex]) {
+    data[pageIndex] = {
+      metadata: page
+    };
+  }
   launchChromeAndRunLighthouse(url, OPTIONS).then(results => {
     const error = results.runtimeError.message;
     if (error) {
@@ -62,13 +91,18 @@ function audit(pages) {
       fs.appendFileSync(ERROR, message);
     } else {
       const categories = Object.values(results.categories);
-      let scores = [];
       for (let category of categories) {
-        scores.push(Math.round(category.score * 100));
+        if (!data[pageIndex].scores) {
+          data[pageIndex].scores = {};
+        }
+        if (!data[pageIndex].scores[category.title]) {
+          data[pageIndex].scores[category.title] = [];
+        }
+        console.log('Pushing score:',category.title, Math.round(category.score * 100));
+        data[pageIndex].scores[category.title].
+          push(Math.round(category.score * 100));
       }
-      const pageScores = `${page},${scores.join(',')}\n`;
-      fs.appendFileSync(OUTPUT, pageScores);
-      console.log(pageScores);
+      // const pageScores = `${page},${scores.join(',')}\n`;
       // If there are still pages to audit, call audit() again.
     }
   }).catch(error => {
@@ -76,9 +110,19 @@ function audit(pages) {
     fs.appendFileSync(ERROR,
       `Caught error for ${url}:\n${error}\n\n`);
   }).finally(() => {
-    if (pages.length) {
+    // If more pages still to audit on this run, begin next audit.
+    // Otherwise if there are more runs to do, begin next run.
+    // Otherwise, write data to output file.
+    if (++pageIndex < pages.length) {
+      console.log('Audit next page');
+      audit(pages);
+    } else if (++runIndex < NUM_RUNS) {
+      console.log('Start run', runIndex + 1);
+      pageIndex = 0;
       audit(pages);
     } else {
+      fs.appendFileSync(OUTPUT, getOutput(data));
+      console.log('\ndata: ', getOutput(data));
       console.log('Completed audit\n');
     }
   });
@@ -93,5 +137,38 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
       return chrome.kill().then(() => results.lhr);
     });
   });
+}
+
+// data is an array of objects: metadata and scores for each URL.
+// This function adds medianScores to each object.
+function getOutput(testResults) {
+  let output = [];
+  console.log('Categories: ', Object.keys(testResults[0].scores));
+  for (const page of testResults) {
+    let pageData = [page.metadata];
+    for (const scores of Object.values(page.scores)) {
+      console.log('scores:', scores);
+      pageData.push(median(scores));
+    }
+    output.push(pageData.join(','));
+  }
+  console.log('output:', output.join(','));
+  return output.join(',');
+}
+
+
+// Utility functions
+
+function median(array) {
+  array = array.sort((a, b) => a - b);
+  if (array.length === 0) {
+    return 0;
+  }
+  var middle = Math.floor(array.length / 2);
+  if (array.length % 2) {
+    return array[middle];
+  } else {
+    return (array[middle - 1] + array[middle]) / 2;
+  }
 }
 
