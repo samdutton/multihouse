a4e63d407442434da24f5cc37e4f50c7aeabd1a1
Sam Dutton <samdutton@gmail.com>
2020-05-16 09:59:20 +0100

    Successfully ran with Web Vitals

--- src/index.js
+++ src/index.js
@@ -27,14 +27,25 @@ let chromeFlags = ['--headless'];
 let inputFile = 'input.csv';
 let numRuns = 3;
 let outputFile = 'output.csv';
-let outputAudits = false;
-let outputVitals = false;
-const metrics = ['time-to-first-byte', 'first-contentful-paint',
+
+// Output data for invididual audits, not just categories.
+let outputAllAudits = false;
+
+// Output Web Vitals data.
+let outputWebVitals = false;
+
+const webVitalsAuditIDs = ['time-to-first-byte', 'first-contentful-paint',
   'largest-contentful-paint', 'speed-index', 'max-potential-fid',
   'first-cpu-idle', 'total-blocking-time', 'cumulative-layout-shift'];
-const metadataAudits = [];
-let onlyCategories =
+const webVitalsTitles = ['TTFB', 'FCP', 'LCP', 'Speed Index', 'FID',
+  'First CPU Idle', 'TBT', 'CLS'];
+// audit titles are collected from Lighthouse results.
+const auditTitles = new Set();
+// Each category is an aggregate score based on multiple audit scores.
+let categories =
   ['performance', 'pwa', 'best-practices', 'accessibility', 'seo'];
+const categoryTitles =
+  ['Performance', 'PWA', 'Best practices', 'Accessibility', 'SEO'];
 let scoreMethod = 'median';
 
 let okToStart = true;
@@ -48,22 +59,22 @@ const argv = require('yargs')
   .alias('m', 'metadata')
   .alias('o', 'output')
   .alias('t', 'audits')
-  .alias('n', 'vitals')
   .alias('r', 'runs')
   .alias('s', 'score-method')
+  .alias('w', 'web-vitals')
   .describe('a', 'Append output to existing data in output file')
-  .describe('c', 'Audits to run: one or more comma-separated values,\n' +
-    'default is:\n' + `${onlyCategories.join(',')}`)
+  .describe('c', 'Categories to test: one or more comma-separated values,\n' +
+    'default is:\n' + `${categories.join(',')}`)
   .describe('f', 'One or more comma-separated Chrome flags *without* dashes,\n' +
     `default is ${chromeFlags}`)
   .describe('i', `Input file, default is ${inputFile}`)
-  .describe('m', 'Headings for optional page metadata')
-  .describe('n', 'Include Web Vitals metrics in audit output')
+  .describe('m', 'Headings for optional page information')
   .describe('o', `Output file, default is ${outputFile}`)
-  .describe('t', `Generate audit scores to output file`)
+  .describe('t', `Include all individual audit scores in output`)
   .describe('r', 'Number of times Lighthouse audits are run for each URL, ' +
     `default is ${numRuns}`)
   .describe('s', `Method of score aggregation, default is ${scoreMethod}`)
+  .describe('w', 'Include Web Vitals in output')
   .help('h')
   .argv;
 
@@ -75,8 +86,8 @@ if (argv.c) {
   const isValid =
     /(performance|pwa|best-practices|accessibility|seo|,)+/.test(argv.c);
   if (isValid) {
-    onlyCategories = argv.c.split(',');
-    console.log(`Auditing categories: ${onlyCategories}`);
+    categories = argv.c.split(',');
+    console.log(`Auditing categories: ${categories}`);
   } else {
     displayError('--c option must be one or more comma-separated values: ' +
       `${argv.c} is not valid`);
@@ -94,15 +105,17 @@ if (argv.i) {
   inputFile = argv.i;
 }
 
-// Headings for optional page metadata.
-// These will be prepended to the CSV output followed by the audit categories.
+// Headings for page information.
+// These will be added to the first line of outputFile.csv,
+// followed by the category headings.
 // For example:
-// Name,Page type,URL,Performance,PWA,Best Practices,Accessibility,SEO
-// This line will be followed by a line for each URL successfully audited.
+// • pageHeadings: 'Name,Page type,URL'
+// • Categories: 'Performance,PWA,Best Practices,Accessibility,SEO'
+// This first line will be followed by a line for each URL successfully audited.
 // For example: John Lewis,homepage,https://johnlewis.com, 32, 40, 78, 87, 100
-let metadataValues = 'Name,Page type,URL';
+let pageHeadings = ['Name', 'Page', 'type', 'URL'];
 if (argv.m) {
-  metadataValues = argv.m;
+  pageHeadings = argv.m.split(',');
 }
 
 if (argv.o) {
@@ -110,11 +123,7 @@ if (argv.o) {
 }
 
 if (argv.t) {
-  outputAudits = true;
-}
-
-if (argv.n) {
-  outputVitals = true;
+  outputAllAudits = true;
 }
 
 if (argv.r) {
@@ -141,10 +150,15 @@ if (argv.v) {
   okToStart = false;
 }
 
+if (argv.w) {
+  outputWebVitals = true;
+  console.log('\nIncluding scores for Web Vitals audits');
+}
+
 const OPTIONS = {
   chromeFlags: chromeFlags,
   // logLevel: 'info'
-  onlyCategories: onlyCategories,
+  categories: categories,
 };
 
 // If required, delete existing output and error data.
@@ -163,117 +177,52 @@ fs.writeFile(ERROR_LOG, '', () => {
 // For example: John Lewis,homepage,https://johnlewis.com,
 // Note that no checks are done on the validity of inputFile or its data.
 const inputFileText = fs.readFileSync(inputFile, 'utf8').trim();
-const inputData = inputFileText.split('\n');
-
-const data = [];
-// okToStart is set to false if the app is being run to get the version number.
-if (okToStart) {
-  audit(inputData);
+const pages = [];
+for (const page of inputFileText.split('\n')) {
+  pages.push({
+    page: page,
+    url: getUrl(page),
+  });
 }
 
-// First two pageParts are website name and page name.
-// URL may contain commas.
+// The page URL is the third item on each line of CSV data.
+// The first two pageParts are website name and page name.
+// URLs may contain commas, hence the join().
 function getUrl(page) {
   const pageParts = page.split(',');
   return pageParts.slice(2, pageParts.length).join();
 }
 
+// okToStart is set to false if the app is being run to get the version number.
+if (okToStart) {
+  audit(pages);
+}
+
 // Run a Lighthouse audit for a web page.
 // The pages parameter is an array of CSV strings, each ending with a URL.
 // For example: John Lewis,homepage,https://johnlewis.com
+const outputData = [];
 function audit(pages) {
   console.log(`\nRun ${runIndex + 1} of ${numRuns}: ` +
-    `URL ${pageIndex + 1} of ${pages.length}`);
+    `page ${pageIndex + 1} of ${pages.length}`);
   // page corresponds to a line of data in the CSV file inputFile.
+  // For example: John Lewis,homepage,https://johnlewis.com
   const page = pages[pageIndex];
-  // The page URL is the last item on each line of CSV data.
-  const url = getUrl(page);
-  launchChromeAndRunLighthouse(url, OPTIONS).then((results) => {
+  launchChromeAndRunLighthouse(page.url, OPTIONS).then((results) => {
     if (results.runtimeError) {
       displayAndWriteError(`Lighthouse runtime error for ` +
-        `${url}.\n\n${results.runtimeError.message}\n`);
+        `${page.url}.\n\n${results.runtimeError.message}\n`);
     } else {
-      // data is an array of objects: metadata and scores for each URL.
-      if (!data[pageIndex]) {
-        data[pageIndex] = {
-          metadata: page,
-        };
+      // If this is the first run for the current page,
+      // add an item for it to results.
+      if (!outputData[pageIndex]) {
+        outputData[pageIndex] = page;
       }
-      // *** Add code here if you want to save complete Lighthouse reports ***
-      const categories = Object.values(results.categories);
-      for (const category of categories) {
-        if (!data[pageIndex].scores) {
-          data[pageIndex].scores = {};
-        }
-        if (!data[pageIndex].scores[category.title]) {
-          data[pageIndex].scores[category.title] = [];
-        }
-        const score = Math.round(category.score * 100);
-        if (score === 0) {
-          displayAndWriteError(`Zero ${category.title} score for ${url}. ` +
-            `This data will be discarded.`);
-        } else {
-          console.log(`${url}: ${category.title} ${score}`);
-          data[pageIndex].scores[category.title].push(score);
-        }
-      }
-
-      if (outputVitals) {
-        console.log('Adding metrics to page test.');
-        for (const metric of metrics) {
-          if (!data[pageIndex].metrics) {
-            data[pageIndex].metrics = {};
-          }
-          if (!data[pageIndex].metrics[metric]) {
-            data[pageIndex].metrics[metric] = [];
-          }
-          const metricValue = results.audits[metric].numericValue;
-          if (metricValue === 0) {
-            displayAndWriteError(`Zero ${results.audits[metric].score} score for ${url}.
-            This data will be discarded.`);
-          } else {
-            console.log(`${url}: ${metric} ${metricValue}`);
-            data[pageIndex].metrics[metric].push(metricValue);
-          }
-        }
-      }
-
-      if (outputAudits) {
-        console.log('Adding audits to output');
-        const audits = Object.values(results.audits);
-        for (const audit of audits) {
-          // Check if metadata for audits not added yet.
-          if (metadataAudits.length < audits.length) {
-            metadataAudits.push(audit.title);
-          }
-
-          if (!data[pageIndex].audits) {
-            data[pageIndex].audits = {};
-          }
-          data[pageIndex].audits[audit.id] = audit.score;
-        }
-      }
+      handleResults(page.url, results);
     }
   }).catch((error) => {
-    displayAndWriteError(`Caught error for ${url}:\n${error}`);
-  }).finally(() => {
-    // If there are more pages to audit on this run, begin the next page audit.
-    if (++pageIndex < pages.length) {
-      audit(pages);
-    // Otherwise, if there are more runs to do, begin the next run.
-    } else if (++runIndex < numRuns) {
-      console.log(`\nStart run ${runIndex + 1}`);
-      pageIndex = 0;
-      audit(pages);
-    // Otherwise, write data to the   t file.
-    } else {
-      // categories is a list of Lighthouse audits completed.
-      // For example: Performance, PWA, Best practices, Accessibility, SEO
-      fs.appendFileSync(outputFile, getOutput(data));
-      console.log(`\nCompleted ${numRuns} run(s) for ${data.length} URL(s)` +
-        `with ${numErrors} error(s).\n\nView output: ${outputFile}\n`);
-    }
-  });
+    displayAndWriteError(`Caught error for ${page.url}:\n${error}`);
+  }).finally(checkIfFinished);
 }
 
 // Launch Chrome, run a Lighthouse audit, then kill Chrome.
@@ -287,49 +236,181 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
   });
 }
 
-// The testResults parameter is an array of objects, one for each URL audited.
-// Each object has median Lighthouse scores and (optional) metadata.
-// This function returns a string in CSV format, each line of which has
-// optional metadata followed by median Lighthouse scores for a URL.
+// Handle results from Lighthouse.
+// results is an array of objects: page info and scores for each URL.
+function handleResults(url, results) {
+  // *** Add code here if you want to save complete Lighthouse reports ***
+  // Provide scores for categories: Performance, PWA, etc.
+  // Each category provides a single aggregate score based on individual audits.
+  addCategoryScores(url, results);
+  // If flag set, provide scores for Web Vitals audits.
+  if (outputWebVitals) {
+    addWebVitalsScores(url, results);
+  }
+  // If flag set, provide data for all invididual audits (not just categories).
+  if (outputAllAudits) {
+    addAuditScores(url, results);
+  }
+}
+
+// Categories provide a single aggregate score for Performance, PWA, etc.
+// Lighthouse calculates category scores based on indvidual audit scores.
+// This app averages category scores over multiple runs, so this function
+// pushes scores to an array for each category.
+function addCategoryScores(url, results) {
+  const categories = Object.values(results.categories);
+  for (const category of categories) {
+    if (!outputData[pageIndex].categoryScores) {
+      outputData[pageIndex].categoryScores = {};
+    }
+    if (!outputData[pageIndex].categoryScores[category.id]) {
+      outputData[pageIndex].categoryScores[category.id] = [];
+    }
+    const score = Math.round(category.score * 100);
+    if (score === 0) {
+      displayAndWriteError(`Zero ${category.title} score for ${url}. ` +
+        `This data will be discarded.`);
+    } else {
+      console.log(`${url}: ${category.title} ${score}`);
+      // Scores are collected for multiple runs,
+      // then a median or average is provided.
+      outputData[pageIndex].categoryScores[category.id].push(score);
+    }
+  }
+}
+
+// Get Web Vitals scores: a subset of audits.
+// See https://web.dev/vitals.
+// TODO: might be possible to combine this with addAuditScores().
+function addWebVitalsScores(url, results) {
+  console.log(`Adding Web Vitals audit scores for ${url}.`);
+  // Check if this audit has already been added to results,
+  // i.e. on a previous run.
+  for (const auditID of webVitalsAuditIDs) {
+    if (!outputData[pageIndex].webVitalsScores) {
+      outputData[pageIndex].webVitalsScores = {};
+    }
+    if (!outputData[pageIndex].webVitalsScores[auditID]) {
+      outputData[pageIndex].webVitalsScores[auditID] = [];
+    }
+    // numericValue is a measured value (such as milliseconds for FCP)
+    // whereas score is a rating between 0 and 1.
+    const numericValue = results.audits[auditID].numericValue;
+    if (numericValue === 0) {
+      displayAndWriteError(`Zero ${results.audits[audit].score} score ` +
+      `for ${url}. This data will be discarded.`);
+    } else {
+      console.log(`${url}: ${auditID} ${numericValue}`);
+      outputData[pageIndex].webVitalsScores[auditID].push(numericValue);
+    }
+  }
+}
+
+// Provide data for all invididual audits, not just aggregate category scores.
+function addAuditScores(url, results) {
+  console.log('Adding all individual audit scores to output.');
+  // Each value of results.audits has information about the audit
+  // and a score for that audit.
+  const audits = Object.values(results.audits);
+  for (const audit of audits) {
+    // TODO: run this once, not for every audit() call.
+    auditTitles.add(audit.title);
+    // Check if this audit has already been added to results,
+    // i.e. on a previous run.
+    if (!outputData[pageIndex].auditScores) {
+      outputData[pageIndex].auditScores = {};
+    }
+    if (!outputData[pageIndex].auditScores[audit.id]) {
+      outputData[pageIndex].auditScores[audit.id] = [];
+    }
+    outputData[pageIndex].auditScores[audit.id].push(audit.score);
+  }
+}
+
+// Check if there are more pages for the current run, or more runs.
+function checkIfFinished() {
+  // If there are more pages to audit in this run, begin the next page audit.
+  if (++pageIndex < pages.length) {
+    audit(pages);
+  // Otherwise, if there are more runs to do, begin the next run.
+  } else if (++runIndex < numRuns) {
+    console.log(`\nStart run ${runIndex + 1}`);
+    pageIndex = 0;
+    audit(pages);
+  // Otherwise, write data to outputFile.
+  } else {
+    // categories is a list of Lighthouse audits completed.
+    // For example: Performance, PWA, Best practices, Accessibility, SEO
+    fs.appendFileSync(outputFile, createOutputCSV(outputData));
+    console.log(`\nCompleted ${numRuns} run(s) for ${outputData.length} URL(s)` +
+      `with ${numErrors} error(s).\n\nView output: ${outputFile}\n`);
+  }
+}
+
+// This function returns a string in CSV format
+// Each line has page info followed by median Lighthouse scores for a URL.
 // For example: John Lewis,homepage,https://johnlewis.com, 32, 40, 78, 87, 100
-function getOutput(testResults) {
+// If the outputAllAudits flag is set, all individual audit scores will be appended.
+// If the outputWebVitals flag is set, Web Vitals scores will be appended.
+// The results parameter is an array of objects, one for each page audited.
+// Each object includes page info and Lighthouse results.
+function createOutputCSV(results) {
   const output = [];
-  for (const page of testResults) {
-    // console.log(page);
-    const pageData = [page.metadata];
-    for (const scores of Object.values(page.scores)) {
+  // Begin outputCSV with column headings.
+  // • Page info headings: Name, page type, URL.
+  // • Category titles: Performance, PWA, etc.
+  // • Web Vitals titles if outputWebVitals flag was set to include Web Vitals scores.
+  // • Audit titles, if outputAllAudits flag was set to include all individual audit scores.
+  // Note that auditTitles is empty unless outputAllAudits flag was set.
+  let outputCSV = outputWebVitals ?
+    [...pageHeadings, ...categoryTitles, ...webVitalsTitles, ...auditTitles].join(',') :
+    [...pageHeadings, ...categoryTitles, ...auditTitles].join(',');
+
+  // Add scores for each page successfully audited.
+  // results is an array of objects, one for each page audited.
+  for (const page of results) {
+    // Ignore pages where Lighthouse couldn't get scores (e.g. for 404 or 403).
+    if (!page.categoryScores) {
+      console.log(`No scores available for ${getUrl(page)}.`);
+      continue;
+    }
+
+    // Create an array of data for the current page.
+    // First item in the arrray is a CSV string with page information.
+    // For example: 'John Lewis,homepage,https://johnlewis.com'.
+    const pageData = [page.page];
+
+    // Each page.categoryScores key is a category.
+    // Each page.categoryScores value is an array of scores from multiple runs.
+    // Push a median or average category score to the pageData array.
+    // Categories (Performance, PWA, etc.) aggregate individual audit scores.
+    for (const categoryScores of Object.values(page.categoryScores)) {
       // Only options at present are median and average
       pageData.push(scoreMethod === 'median' ?
-        median(scores) : average(scores));
+        median(categoryScores) : average(categoryScores));
     }
 
-    if (outputVitals) {
-      for (const metricScores of Object.values(page.metrics)) {
-        // Only options at present are median and average
+    // If flag set, append all individual audit scores for the current page.
+    if (outputAllAudits) {
+      for (const auditScores of Object.values(page.auditScores)) {
         pageData.push(scoreMethod === 'median' ?
-          median(metricScores) : average(metricScores));
+          median(auditScores) : average(auditScores));
       }
-    }
-
-    if (outputAudits) {
-      for (const audit of Object.values(page.audits)) {
-        // Only options at present are median and average
-        pageData.push(audit);
+    // If flag set, add Web Vitals scores for the current page.
+    // Web Vitals are a selection of audit scores.
+    } else if (outputWebVitals) {
+      // webVitalsScores is an array of results for a Web Vitals metric.
+      for (const webVitalsScores of Object.values(page.webVitalsScores)) {
+        // Only options at present are median and average.
+        pageData.push(scoreMethod === 'median' ?
+          median(webVitalsScores) : average(webVitalsScores));
       }
     }
     output.push(pageData.join(','));
   }
-  // Prepend CSV data with headings and audit categories.
-  // For example: Name,Page type,URL,Performance,PWA, Accessibility,SEO
-  const categories = Object.keys(data[0].scores).join(',');
-  const audits = metadataAudits.join(',');
-  if (outputAudits) {
-    return `${metadataValues},${categories},${audits}\n${output.join('\n')}`;
-  } else if (outputVitals) {
-    return `${metadataValues},${categories},${metrics}\n${output.join('\n')}`;
-  } else {
-    return `${metadataValues},${categories}\n${output.join('\n')}`;
-  }
+
+  outputCSV += `\n${output.join('\n')}`;
+  return outputCSV;
 }
 
 
